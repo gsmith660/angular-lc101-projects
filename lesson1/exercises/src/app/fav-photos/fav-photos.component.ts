e2f7b5147a8cc97dd56f860fbdb2916a050890f7
gsmith660 <gsmith660@gmail.com>
2019-12-10 16:50:46 -0600

    Part 1 Completed

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Photo Gallery';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://upload.wikimedia.org/wikipedia/commons/7/7d/Trunc-icosa.jpg';
+  image3 = 'https://upload.wikimedia.org/wikipedia/commons/1/12/Knot_table.svg';
 
   constructor() { }
 
